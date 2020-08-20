4376449637b288e8f1a6b787a67098ce56f983b1
Archit Vasan <akvasan2@detroit.ks.uiuc.edu>
2020-08-20 10:58:32 -0500

    comments for easy user accessibility

--- MCPS.py
+++ MCPS.py
@@ -1,7 +1,8 @@
 ######################## Monte Carlo Based Pathway Search Algorithm ##############################
-######################## Contributors: Nandan Haloi and Archit Vasan ############################
+
+######################## Contributors: Archit Vasan and Nandan Haloi ############################
 ######################## Affiliation: University of Illinois at Urbana-Champaign########################
-####################### Emails: nhaloi2@illinois.edu, akvasan2@illinois.edu ######################
+####################### Emails: akvasan2@illinois.edu, nhaloi2@illinois.edu, ######################
 
 ############### Code description: Method to use docking data to obtain a pathway which is connected (has no large jumps)#############################
 
@@ -9,6 +10,7 @@
 ################################# The z-coordinates are used as a primary coordinate to define the pathway progressioni while the angles restrict exploration of the path.
 ################################# Moves are attempted using Metropolis criterion.
 
+####################################################################################################################################################
 
 import numpy as np
 import math as m
@@ -73,7 +75,7 @@ def allowed_frames(data,z_list,memory_array,prev_frame,inc_ind,az_ind,inc_step,a
 		next_az=data[n_f][az_ind]
 
 
-		########## Move in multiple angles in one step ####################
+		########## Move in multiple angles in one step. only check frame if its memory_array value is 0 (not previously accepted) ####################
 		if (indep==0 and memory_array[n_f]==0):
 
                         ######### if azimuthal bottom boundary is less than 0:
@@ -174,7 +176,7 @@ def allowed_frames(data,z_list,memory_array,prev_frame,inc_ind,az_ind,inc_step,a
 				allowed_fr=np.append(allowed_fr,n_f)
 				allowed_fr_z_bin[n_f]=z_i+1
 
-	return allowed_fr,allowed_fr_z_bin
+	return allowed_fr,allowed_fr_z_bin ########### return both the frames as well as the z bin each frame belongs to
 
 ############### Procedure to perform actual Monte Carlo Based Pathway Search #############################
 
@@ -184,14 +186,14 @@ def Transition_Search(data,z_list,output_file,N_zbins,inc_step,az_step,small_dev
 
 	N_frames=len(data) ### Total poses in docking data
 	memory_array=np.zeros(N_frames) #### array used to check if frame has already been accepted
-	total_en_unstand=data[:,energy_ind]    #### Energies 
-	en_stand=standardize_energy(total_en_unstand)   #### Standardize all total energies
+	total_en_unstand=data[:,energy_ind]    
+	en_stand=standardize_energy(total_en_unstand)   #### Standardize all total energies to use in algorithm
 
 	z_i = 0 ############ start in 0th (top) z bin
 	acc_frames=[]  ######## initialize array holding accepted frames
 	acc_energies=[] ######## initialize array holding enery of acc frame
 
-############ Pick initial frame in topmost z bin ###########################
+############ Pick random initial frame in topmost z bin ###########################
 
 	seed(iteration)  ####### seed using user provided iteration
 	rand=randint(0,len(z_list[0])-1)   ###### pick a random position in the beginning to start
@@ -201,39 +203,56 @@ def Transition_Search(data,z_list,output_file,N_zbins,inc_step,az_step,small_dev
 
 	acc_frames=np.append(acc_frames,start_fr)  ####### add random frame to accepted frames array
 
-	step_num=0
+############# While loop which performs pathway search until the last primary coordinate (z) bin is reached ###################
+
+	step_num=0  ###### step along path identified so far
+
 	while (z_i<N_zbins-1):
 
-		prev_frame=acc_frames[int(step_num)]
+		prev_frame=acc_frames[int(step_num)] ########## previous frame identified in path
 		prev_en=en_stand[int(prev_frame)]
 
-		allowed_frames_proc=allowed_frames(data,z_list,memory_array,prev_frame,inc_ind,az_ind,inc_step,az_step,small_dev_inc,small_dev_az,z_i,indep=0)
+################ Use above allowed_frames procedure to create allowed frames for next step of algorithm ##################
+
+		allowed_frames_proc=allowed_frames(data,z_list,memory_array,prev_frame,inc_ind,az_ind,inc_step,az_step,small_dev_inc,small_dev_az,z_i,indep=0) 
 		allowed_fr=allowed_frames_proc[0]
 		allowed_fr_z_bin=allowed_frames_proc[1]
 		allowed_fr_num=len(allowed_fr)
+
+		####### if no frames are available, termainate pathway search ########
+
 		if (allowed_fr==[]):
-			#print(acc_frames)
 			return 0
 
 		accepted=0
 		rejected=0
 		bad_energy=0
 
+################ Perform Metropolis search until a pose is accepted #############################
+
 		while (accepted==0):
+			######## if no frames allowed to be checked anymore, terminate pathway #######
 			if(rejected>=allowed_fr_num or allowed_fr_num==[]):
 				return 0
+			####### choose random frame from allowed_fr (fr_try) to check acceptance 
 
 			index=randint(0,int(len(allowed_fr)-1))
 			fr_try=int(allowed_fr[index])
 			en_try=en_stand[fr_try]
 
-			met_crit=m.exp(prev_en-en_try)
+			met_crit=m.exp(prev_en-en_try) #Metropolis relation between previous energy and new one.
+
+			###### automatically accept if met_crit >=1
+			#####  if accepted, add to memory array so it wont be checked again, update z_i to new z bin, increase step_num
 			if met_crit>=1:
 				accepted+=1
 				acc_frames=np.append(acc_frames,fr_try)
 				z_i=allowed_fr_z_bin[fr_try]
 				memory_array[fr_try]=1
 				step_num+=1
+
+			######### otherwise spit random number between 0, 1. if metrop> random number, accept
+
 			else:
 				rand=np.random.uniform(0,1)
 				if met_crit>rand:
@@ -244,11 +263,11 @@ def Transition_Search(data,z_list,output_file,N_zbins,inc_step,az_step,small_dev
 					step_num+=1
 				else:
 					it=np.where(allowed_fr==fr_try)
-					allowed_fr=np.delete(allowed_fr,it)
-					memory_array[fr_try]=1
+					allowed_fr=np.delete(allowed_fr,it) ##### remove from list of allowed poses
+					memory_array[fr_try]=1 #### removing from memory if rejected as well right now
 					rejected+=1
-					#print(z_i)
 					continue
+############### once a path is accepted write to output file ########################
 	f=open(output_file,'a+',newline='')
 	writer=csv.writer(f,delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
 	writer.writerow(acc_frames)
